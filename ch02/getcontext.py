5565ab20ab44c01b65a84bd7fcb09d295c56e62d
allen-5183 <allen5183@gmail.com>
2026-07-21 16:14:24 +0800

    20260721 Python 程式設計

---
+++ ch02/getcontext.py
@@ -0,0 +1,10 @@
+import decimal  #匯入decimal模組
+d1=decimal.Decimal.from_float(123.4567)
+d2=decimal.Decimal.from_float(34.5678)
+print(d1+d2)
+
+print(decimal.getcontext()) # 取出目前 decimal 資料型別運算的設定值
+print(decimal.getcontext().prec)
+print(decimal.getcontext().rounding)
+decimal.getcontext().prec=8
+print(d1+d2)
---
+++ ch02/variable.py
@@ -0,0 +1,11 @@
+yesNo='y'
+pssScore, maxScore, minScore = 85, 100, 0
+power1 = power2 = 100
+total = 1.23456E+6  # 123456.0
+print(yesNo)
+print(pssScore, maxScore, minScore)
+print(power1, power2)
+print(total)
+del total
+# print(total) # 報錯，  已經刪除
+
